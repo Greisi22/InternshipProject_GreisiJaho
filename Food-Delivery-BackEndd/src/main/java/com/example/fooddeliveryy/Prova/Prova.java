2479bb55fa7bb7726828ddbea3cb4946903a5674
greisi jaho <gjaho21@epoka.edu.al>
2024-03-12 19:57:48 +0100

    Frist commit

---
+++ Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Prova/Prova.java
@@ -0,0 +1,13 @@
+package com.example.fooddeliveryy.Prova;
+
+public class Prova {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
---
+++ Food-Delivery-BackEndd/src/test/java/com/example/fooddeliveryy/FoodDeliveryyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fooddeliveryy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

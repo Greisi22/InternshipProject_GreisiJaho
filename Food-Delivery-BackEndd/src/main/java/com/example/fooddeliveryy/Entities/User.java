23b614ed76b95337c3bc36592b289c3175c0a036
David Keci <dkeci21@epoka.edu.al>
2024-03-17 00:07:01 +0100

    Fixing login with map

--- Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Entities/User.java
+++ Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Entities/User.java
@@ -1,79 +1,79 @@
-//package com.example.fooddeliveryy.Entities;
-//
-//import jakarta.persistence.*;
-//
-//@Entity
-//public class User {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int UserId;
-//
-//    private String userEmail;
-//    private String userPassword;
-//    private String userRole;
-//
-//    public User(){
-//
-//    }
-//
-//    public User(String userEmail, String userPassword) {
-//        userEmail = userEmail;
-//        this.userPassword = userPassword;
-//    }
-//
-//
-//    public User(String userEmail, String userPassword, String userRole) {
-//        this.userEmail = userEmail;
-//        this.userPassword = userPassword;
-//        this.userRole = userRole;
-//    }
-//
-//    public User(int userId, String userEmail, String userPassword, String userRole) {
-//        UserId = userId;
-//        userEmail = userEmail;
-//        this.userPassword = userPassword;
-//        this.userRole = userRole;
-//    }
-//
-//    public int getUserId() {
-//        return UserId;
-//    }
-//
-//    public void setUserId(int userId) {
-//        UserId = userId;
-//    }
-//
-//    public String getUserEmail() {
-//        return userEmail;
-//    }
-//
-//    public void setUserEmail(String userEmail) {
-//        userEmail = userEmail;
-//    }
-//
-//    public String getUserPassword() {
-//        return userPassword;
-//    }
-//
-//    public void setUserPassword(String userPassword) {
-//        this.userPassword = userPassword;
-//    }
-//
-//    public String getUserRole() {
-//        return userRole;
-//    }
-//
-//    public void setUserRole(String userRole) {
-//        this.userRole = userRole;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "User{" +
-//                "UserId=" + UserId +
-//                ", UserEmail='" + userEmail + '\'' +
-//                ", userPassword='" + userPassword + '\'' +
-//                ", userRole='" + userRole + '\'' +
-//                '}';
-//    }
-//}
+package com.example.fooddeliveryy.Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int UserId;
+
+    private String userEmail;
+    private String userPassword;
+    private String userRole;
+
+    public User(){
+
+    }
+
+    public User(String userEmail, String userPassword) {
+        userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
+
+
+    public User(String userEmail, String userPassword, String userRole) {
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userRole = userRole;
+    }
+
+    public User(int userId, String userEmail, String userPassword, String userRole) {
+        UserId = userId;
+        userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userRole = userRole;
+    }
+
+    public int getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(int userId) {
+        UserId = userId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "UserId=" + UserId +
+                ", UserEmail='" + userEmail + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", userRole='" + userRole + '\'' +
+                '}';
+    }
+}

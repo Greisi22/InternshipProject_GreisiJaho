90051dab443069143f711ce9ccab152d0031741a
David Keci <dkeci21@epoka.edu.al>
2024-03-17 00:06:38 +0100

    ignore

--- Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Controllers/LogIn/loginController.java
+++ Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Controllers/LogIn/loginController.java
@@ -1,77 +1,77 @@
-package com.example.fooddeliveryy.Controllers.LogIn;
-
-import com.example.fooddeliveryy.Entities.User;
-import com.example.fooddeliveryy.Services.LogIn.logInService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-@RequestMapping
-@RestController
-public class loginController {
-
-    @Autowired
-    public final logInService logInservice;
-
-    public loginController(logInService logInservice) {
-        this.logInservice = logInservice;
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody Map<String, Object> requestBody) {
-        String email = (String) requestBody.get("email");
-        String password = (String) requestBody.get("password");
-        User userCredentials = null;
-        Map<String, Object> map = new HashMap<>();
-        try {
-            userCredentials = logInservice.checkCredintials(email, password);
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
-            map.put("message", "An error has accured");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(map);
-        }
-        if (userCredentials != null) {
-            map.put("message", userCredentials);
-            return ResponseEntity.status(HttpStatus.OK).body(map);
-        } else {
-            map.put("message", "Invalid credentials");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(map);
-        }
-    }
-
-
-    @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody Map<String, Object> requestBody) {
-        String email = (String) requestBody.get("email");
-        String password = (String) requestBody.get("password");
-        String role = (String) requestBody.get("role");
-        Map<String, String> map = new HashMap<>();
-
-        if (role == null || email == null || password == null) {
-            if(role == null){
-                map.put("message", "The argument role is missing");
-            } else if (email == null) {
-                map.put("message", "The argument email is missing");
-            }else {
-                map.put("message", "The argument password is missing");
-            }
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map.toString());
-        }
-        String message = logInservice.registerUser(email, password, role);
-        if(message == null){
-            map.put("message", "Register was successful!");
-            return ResponseEntity.status(HttpStatus.OK).body(map.toString());
-        }else {
-            map.put("message", message);
-            return ResponseEntity.status(HttpStatus.IM_USED).body(map.toString());
-        }
-
-    }
-
-
-}
+//package com.example.fooddeliveryy.Controllers.LogIn;
+//
+//import com.example.fooddeliveryy.Entities.User;
+//import com.example.fooddeliveryy.Services.LogIn.logInService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@RequestMapping
+//@RestController
+//public class loginController {
+//
+//    @Autowired
+//    public final logInService logInservice;
+//
+//    public loginController(logInService logInservice) {
+//        this.logInservice = logInservice;
+//    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<?> login(@RequestBody Map<String, Object> requestBody) {
+//        String email = (String) requestBody.get("email");
+//        String password = (String) requestBody.get("password");
+//        User userCredentials = null;
+//        Map<String, Object> map = new HashMap<>();
+//        try {
+//            userCredentials = logInservice.checkCredintials(email, password);
+//        } catch (Exception e) {
+//            System.out.println("Error: " + e);
+//            map.put("message", "An error has accured");
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(map);
+//        }
+//        if (userCredentials != null) {
+//            map.put("message", userCredentials);
+//            return ResponseEntity.status(HttpStatus.OK).body(map);
+//        } else {
+//            map.put("message", "Invalid credentials");
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(map);
+//        }
+//    }
+//
+//
+//    @PostMapping("/register")
+//    public ResponseEntity<String> register(@RequestBody Map<String, Object> requestBody) {
+//        String email = (String) requestBody.get("email");
+//        String password = (String) requestBody.get("password");
+//        String role = (String) requestBody.get("role");
+//        Map<String, String> map = new HashMap<>();
+//
+//        if (role == null || email == null || password == null) {
+//            if(role == null){
+//                map.put("message", "The argument role is missing");
+//            } else if (email == null) {
+//                map.put("message", "The argument email is missing");
+//            }else {
+//                map.put("message", "The argument password is missing");
+//            }
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map.toString());
+//        }
+//        String message = logInservice.registerUser(email, password, role);
+//        if(message == null){
+//            map.put("message", "Register was successful!");
+//            return ResponseEntity.status(HttpStatus.OK).body(map.toString());
+//        }else {
+//            map.put("message", message);
+//            return ResponseEntity.status(HttpStatus.IM_USED).body(map.toString());
+//        }
+//
+//    }
+//
+//
+//}
--- Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Repositories/logInRepo.java
+++ Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Repositories/logInRepo.java
@@ -1,15 +1,15 @@
-package com.example.fooddeliveryy.Repositories;
-
-import com.example.fooddeliveryy.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import java.util.Optional;
-
-@Repository
-public interface logInRepo extends JpaRepository<User, Integer> {
-    Optional<User> findByUserEmail(String email);
-
-    boolean existsByUserEmail(String email);
-}
+//package com.example.fooddeliveryy.Repositories;
+//
+//import com.example.fooddeliveryy.Entities.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+//import org.springframework.stereotype.Repository;
+//import java.util.Optional;
+//
+//@Repository
+//public interface logInRepo extends JpaRepository<User, Integer> {
+//    Optional<User> findByUserEmail(String email);
+//
+//    boolean existsByUserEmail(String email);
+//}

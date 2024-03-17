4911ef3ae055a7c4044cba5f711f56e54ffa2bfe
David Keci <dkeci21@epoka.edu.al>
2024-03-17 14:25:28 +0100

    Changing title name

--- Food-Delivery-FrontEnd/Food-Delivery/tailwind.config.js
+++ Food-Delivery-FrontEnd/Food-Delivery/tailwind.config.js
@@ -7,16 +7,30 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        primary: '#1C1562',
-        secondary: '#18A0FB',
-        accent: '#EDEBFF',
-        PrimaryBtn:'#0078AC',
-        deepGreen: '#095E17',
-        primaryGray: '#4D4D4D'
-      }
-
+        white: "#fff",
+        black: "#000",
+        gainsboro: "#e6e6e6",
+        darkslategray: "#454545",
+        dimgray: "rgba(91, 77, 77, 0.31)",
+      },
+      spacing: {
+        "spacing-xs": "8px",
+        "spacing-s": "24px",
+        "spacing-m": "48px",
+      },
+      fontFamily: {
+        subheading: "Inter",
+      },
     },
+    fontSize: {
+      base: "16px",
+      "5xl": "24px",
+      xl: "20px",
+      inherit: "inherit",
+    },
+  },
+  corePlugins: {
+    preflight: false,
   },
   plugins: [],
   darkMode: 'class',

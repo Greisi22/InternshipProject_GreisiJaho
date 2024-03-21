e276b69b6e479f79ff2ae8178f0e289a72ce0453
David Keci <dkeci21@epoka.edu.al>
2024-03-21 22:13:45 +0100

    Ignore

--- Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Controllers/ProductController.java
+++ Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Controllers/ProductController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/product")
 public class ProductController {
@@ -38,6 +40,16 @@ public class ProductController {
             return ResponseEntity.notFound().build();
         }
     }
+    @GetMapping("/get/all")
+    public ResponseEntity<?> getAllProduct(@PathVariable long productId) {
+        List<Product> product = productService.getAllProducts();
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 
 
     @PutMapping("/update/{productId}")
--- Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Services/ProductService.java
+++ Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Services/ProductService.java
@@ -5,6 +5,8 @@ import com.example.fooddeliveryy.Repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductService {
 
@@ -34,6 +36,10 @@ public class ProductService {
             return productRepository.save(existingProduct);
         }
         return null;
+    }
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
     }
 
     public boolean deleteProduct(long productId) {

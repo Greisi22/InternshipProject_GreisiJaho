fe7c64267820d5279bfca02f83c2175eaf5594a0
greisi jaho <gjaho21@epoka.edu.al>
2024-03-27 23:32:07 +0100

    Building the documentation of cache

--- Food-Delivery-FrontEnd/Food-Delivery/src/api/localhost/EntryPage/RetreiveDiscountRestaurant.tsx
+++ Food-Delivery-FrontEnd/Food-Delivery/src/api/localhost/EntryPage/RetreiveDiscountRestaurant.tsx
@@ -1,7 +1,9 @@
 import axiosInstance, { ApiResponse } from '../../../config/axios';
 
+//Retrieving data from the database with axios dependency
 export async function retrieveDiscountRestaurant() {
   try {
+    //Creating a variable that will contain the data that we need from the endpoint API
     const response = await axiosInstance.get<ApiResponse>('/restaurant/discount');
     console.log("THis is response: " , response.data)
     return response.data;

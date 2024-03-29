2556cc79b4c16931d2ae0da246b0189a63b42cb9
David Keci <dkeci21@epoka.edu.al>
2024-03-30 00:12:27 +0100

    finishing with component style

--- Food-Delivery-FrontEnd/Food-Delivery/src/component/General/Menu.tsx
+++ Food-Delivery-FrontEnd/Food-Delivery/src/component/General/Menu.tsx
@@ -2,25 +2,32 @@ import Menu from '@mui/joy/Menu';
 import MenuButton from '@mui/joy/MenuButton';
 import MenuItem from '@mui/joy/MenuItem';
 import Dropdown from '@mui/joy/Dropdown';
+import "./styles/Menu.css"
+import ReorderIcon from '@mui/icons-material/Reorder';
 
+interface MenuItem {
+  icon: JSX.Element | null;
+  name: string;
+}
 
+interface Props {
+  items: MenuItem[];
+}
 
+export default function NavMenu({ items }: Props) {
 
-
-export default function NavMenu({ items }) {
-
-  const [title, ...menuItems] = items;
+  const [, ...menuItems] = items;
 
   return (
     <div className="bg-transparent">
       <Dropdown>
         <MenuButton style={{ backgroundColor: 'transparent', border: 'none' }}>
-          <span>{title.icon}</span> <span style={{marginTop:"-12px"}}>{title.name}</span>
+          <span><ReorderIcon/></span> 
         </MenuButton>
-        <Menu>
+        <Menu style={{backgroundColor: "", border: ""}}>
           {menuItems.map((item, index) => (
-            <div key={index}>
-              <MenuItem>
+            <div key={index} >
+              <MenuItem className='menuItem'>
                 <span>{item.icon}</span> <span>{item.name}</span>
               </MenuItem>
             </div>

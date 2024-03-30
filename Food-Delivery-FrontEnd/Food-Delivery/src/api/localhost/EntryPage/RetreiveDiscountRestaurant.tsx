61edbfb381568b6f731bf5cc9a53aea847b93df2
greisi jaho <gjaho21@epoka.edu.al>
2024-03-30 02:10:27 +0100

    Popularcategories

--- Food-Delivery-FrontEnd/Food-Delivery/src/api/localhost/EntryPage/RetreiveDiscountRestaurant.tsx
+++ Food-Delivery-FrontEnd/Food-Delivery/src/api/localhost/EntryPage/RetreiveDiscountRestaurant.tsx
@@ -2,13 +2,13 @@ import axiosInstance, { ApiResponse } from '../../../config/axios';
 
 //Retrieving data from the database with axios dependency
 export async function retrieveDiscountRestaurant() {
-  try {
-    //Creating a variable that will contain the data that we need from the endpoint API
-    const response = await axiosInstance.get<ApiResponse>('/restaurant/discount');
-    console.log("THis is response: " , response.data)
-    return response.data;
-  } catch (error) {
-    console.log('error: ', error);
-    throw new Error('Failed to fetch data');
-  }
+    try {
+        //Creating a variable that will contain the data that we need from the endpoint API
+        const response = await axiosInstance.get<ApiResponse>('/restaurant/discount');
+        console.log('THis is response: ', response.data);
+        return response.data;
+    } catch (error) {
+        console.log('error: ', error);
+        throw new Error('Failed to fetch data');
+    }
 }
--- Food-Delivery-FrontEnd/Food-Delivery/src/pages/EntryPage.tsx
+++ Food-Delivery-FrontEnd/Food-Delivery/src/pages/EntryPage.tsx
@@ -1,12 +1,13 @@
 import { FunctionComponent } from 'react';
 import DiscountRestaurants from 'src/component/EntryPage/DiscountRestaurants';
+import PopularCategories from 'src/component/EntryPage/PopularCategories';
 import './styles/entry.css';
-
 
 const EntryPage: FunctionComponent = () => {
     return (
         <div className="entryContainer">
-        <DiscountRestaurants />
+            <DiscountRestaurants />
+            <PopularCategories />
         </div>
     );
 };

b6e275441758f43f6c8ff42a319f0988acbb47e2
Rigersa <rbicaku21@epoka.edu.al>
2024-04-02 18:58:04 +0200

    opening page edit

--- Food-Delivery-FrontEnd/Food-Delivery/src/component/EntryPage/BelowHeader.tsx
+++ Food-Delivery-FrontEnd/Food-Delivery/src/component/EntryPage/BelowHeader.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './style/BelowHeader.css';
 import driverImage from '../../assets/motorist-removebg-preview.png'; // Import the image
-import { StarIcon } from '@heroicons/react/24/solid'; // Import the StarIcon
 
 const BelowHeader: React.FC = () => {
   return (
@@ -9,24 +8,13 @@ const BelowHeader: React.FC = () => {
       <div className="content">
         <h1 className="main-heading">
           <span className="best-text">BEST</span> 
+          </h1>
+        <h2 className="main-heading2">
           <span className="food-text">Food</span> <br /> 
           <span className="delivery-text">Delivery</span>
-        </h1>
+          </h2>
         <p className="subheading">Delivering your cravings at lightning speed.</p>
         <button className="order-button">Order Now</button>
-        <div className="rating">
-          <p className="rating-text">
-            <span className="star-icons">
-              <StarIcon className="h-5 w-5 text-yellow-400" />
-              <StarIcon className="h-5 w-5 text-yellow-400" />
-              <StarIcon className="h-5 w-5 text-yellow-400" />
-              <StarIcon className="h-5 w-5 text-yellow-400" />
-              <StarIcon className="h-5 w-5 text-yellow-400" />
-            </span>
-            5-star rating <br />
-            based on 1788 reviews
-          </p>
-        </div>
       </div>
       <div className="image-container">
         <img className="driver-image" src={driverImage} alt="Driver" />

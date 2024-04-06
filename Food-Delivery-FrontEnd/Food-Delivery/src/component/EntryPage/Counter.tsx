8d30dfb2b5c7edaf476ffcdf45ad87343d5f1add
Tea Begaj <teabegaj2002@gmail.com>
2024-04-06 14:24:47 +0200

    Fixed some things

--- Food-Delivery-FrontEnd/Food-Delivery/src/component/EntryPage/Counter.tsx
+++ Food-Delivery-FrontEnd/Food-Delivery/src/component/EntryPage/Counter.tsx
@@ -4,15 +4,15 @@ import './style/CounterCss.css';
 const Counter = () => {
   return (
     <>
-    <div className="w-full h-[20vh] bg-red-200 p-[15px] pl-[80px] pr-[80px] pt-[4px] mt-[100px] first">
+    <div className="w-full h-[15vh] p-[15px] pl-[80px] pr-[80px] pt-[4px] mt-[100px] first">
       <div className="pt- bg-[#e94339] text-center h-full w-full flex rounded-tl-md rounded-tr-md rounded-bl-md rounded-br-md div0 justify-around align-middle">
 
-        <div className="text-white w-[25%] m-auto border-r border-white pr-4"> 
+        <div className="text-white w-[25%] m-auto border-r border-white pr-4 reg"> 
           <h2 className="text-3xl text-white poppins-light-64">546+</h2>
           <p className="py-2 text-white poppins-bold-20">Registered Riders</p>
         </div>
 
-        <div className="text-white w-[25%] m-auto border-r border-white pr-4"> 
+        <div className="text-white w-[25%] m-auto border-r border-white pr-4 orders"> 
           <h2 className="text-3xl text-white poppins-light-64">789,900+</h2>
           <p className="py-2 text-white poppins-bold-20">Orders Delivered</p>
         </div>
@@ -29,10 +29,10 @@ const Counter = () => {
       </div>
     </div>
 
-    <div className="w-full h-[20vh] bg-red-200 p-[40px] pl-[80px] pr-[80px] pt-[4px] mt-[0px] first div4">
+    <div className="w-full h-[15vh] bg-red-200 p-[40px] pl-[80px] pr-[80px] pt-[4px] first div4">
       <div className="pt- bg-[#e94339] text-center h-full w-full flex rounded-tl-md rounded-tr-md rounded-bl-md rounded-br-md justify-around align-middle">
 
-        <div className="text-white w-[25%] m-auto border-r border-white pr-4"> 
+        <div className="text-white w-[25%] m-auto border-r border-white pr-4 rest"> 
           <h2 className="text-3xl text-white poppins-light-64">690+</h2>
           <p className="py-2 text-white poppins-bold-20">Restaurants Partnered</p>
         </div>

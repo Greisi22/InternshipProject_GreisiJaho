42fa7344a3fdf7104e32e0bd354e77f520b33cac
David Keci <dkeci21@epoka.edu.al>
2024-04-26 00:35:27 +0200

    Adding login register and finished sicurity login

--- Food-Delivery-FrontEnd/Food-Delivery/src/pages/EntryPage.tsx
+++ Food-Delivery-FrontEnd/Food-Delivery/src/pages/EntryPage.tsx
@@ -10,52 +10,62 @@ import Entry from 'src/component/EntryPage/Entry';
 import EntryInfo from 'src/component/EntryPage/EntryInfo';
 import ChangeImages from 'src/component/EntryPage/ChangeImages';
 import NavBar from 'src/component/General/NavBar';
+import Register from 'src/component/Login/Register';
+import Login from 'src/component/Login/Login';
 
 const EntryPage: FunctionComponent = () => {
     return (
-        <div className="entryContainer">
-            <div id="Home" className='mt-[30px]'>
-                  {/* <NavBar /> */}
-            <Entry />
-               {/* <ChangeImages/> */}
-             </div>
-         
-           
+        <>
+            <div className="entryContainer">
+                {/* <div className="absolute h-full  w-full  z-[100] mt-[-30px]"> 
+                    <Register />
+                </div> */}
 
-            <br />
-            <br />
-            <div id="Discounts">
-                {/* <DiscountRestaurants /> */}
-            </div>
-            <br />
-            <br />
-            <div id="Popular">
-                <PopularCategories />
-            </div>
-            <br />
-            <br />
+                {/* <div className="absolute h-full  w-full  z-[100] mt-[-30px]"> 
+                    <Login />
+                </div> */}
 
-            <PopularRestaurants />
+                <div id="Home" className="mt-[30px]">
+                    {/* <NavBar /> */}
+                    <Entry />
+                    {/* <ChangeImages/> */}
+                </div>
 
-            <br />
-            <br />
-            <HowToOrder />
-            <br />
-            <br />
-            <div id="Services">
-                <UserReview />
+                <br />
+                <br />
+                <div id="Discounts">
+                    <DiscountRestaurants />
+                </div>
+                <br />
+                <br />
+                <div id="Popular">
+                    <PopularCategories />
+                </div>
+                <br />
+                <br />
+
+                <PopularRestaurants />
+
+                <br />
+                <br />
+                <HowToOrder />
+                <br />
+                <br />
+                <div id="Services">
+                    <UserReview />
+                </div>
+                <br />
+                <br />
+                <EntryInfo />
+                <br />
+                <br />
+                <br />
+                <div id="Contact">
+                    {' '}
+                    <Footer />
+                </div>
             </div>
-            <br />
-            <br />
-            <EntryInfo />
-            <br />
-            <br />
-            <br />
-            <div id="Contact">
-                {' '}
-                <Footer />
-            </div>
-        </div>
+        </>
     );
 };
 

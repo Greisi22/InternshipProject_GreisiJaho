691112ce46f6907b20b57ff89ba994f897fbde73
David Keci <dkeci21@epoka.edu.al>
2024-05-01 23:04:13 +0200

    Creating Payment and Revenue endpoints

--- Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Services/ReviewService.java
+++ Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Services/ReviewService.java
@@ -1,13 +1,13 @@
 package com.example.fooddeliveryy.Services;
 
-import com.example.fooddeliveryy.Entities.Rastaurant;
 import com.example.fooddeliveryy.Entities.Review;
 import com.example.fooddeliveryy.Repositories.ReviewRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ReviewService {
@@ -19,11 +19,10 @@ public class ReviewService {
 
     public Review createReview(Review review) {
         if (review != null) {
-           return reviewRepository.save(review);
+            return reviewRepository.save(review);
         }
         return null;
     }
-
 
 
     public List<Review> getReviewsGreaterThanFive() {
@@ -40,15 +39,37 @@ public class ReviewService {
     }
 
 
-
-        public List<Review> getReviewsByDate(int year, int month, int day) {
-            LocalDate date = LocalDate.of(year, month, day);
-            return reviewRepository.findByDate(date);
-        }
+    public List<Review> getReviewsByDate(int year, int month, int day) {
+        LocalDate date = LocalDate.of(year, month, day);
+        return reviewRepository.findByDate(date);
+    }
 
     public List<Review> getReviewsForRestaurant(long restaurantId) {
         return reviewRepository.findByRestaurantId(restaurantId);
     }
+
+
+    public Map<Integer, Long> getReviewCountsByRatingAndDateRange(LocalDate startDate, LocalDate endDate) {
+        Map<Integer, Long> ratingCounts = new HashMap<>();
+
+        for (int rating = 1; rating <= 5; rating++) {
+            long count = reviewRepository.countByRatingAndDateBetween(rating, startDate, endDate);
+            ratingCounts.put(rating, count);
+        }
+
+        return ratingCounts;
     }
+
+
+    public Map<Integer, Long> getReviewCountsByRating() {
+        Map<Integer, Long> ratingCounts = new HashMap<>();
+        for (int rating = 1; rating <= 5; rating++) {
+            long count = reviewRepository.countByRating(rating);
+            ratingCounts.put(rating, count);
+        }
+
+        return ratingCounts;
+    }
+}
 
 

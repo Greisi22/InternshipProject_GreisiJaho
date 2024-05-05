e804e2929192367213446e63d5a9314a0fd7cff0
greisi jaho <gjaho21@epoka.edu.al>
2024-05-05 23:00:29 +0200

    Ignore

---
+++ Food-Delivery-FrontEnd/Food-Delivery/src/api/localhost/Administrator/restaurantsApi.tsx
@@ -0,0 +1,40 @@
+import axiosInstance, { ApiResponse } from '../../../config/axios';
+
+//Retrieving data from the database with axios dependency
+export async function retrieveAllRestaurant() {
+    try {
+        //Creating a variable that will contain the data that we need from the endpoint API
+        const response = await axiosInstance.get<ApiResponse>('/restaurant/get/all');
+        console.log("Response: ",response)
+        return response.data;
+    } catch (error) {
+        console.log('error: ', error);
+        throw new Error('Failed to fetch data');
+    }
+}
+
+
+export async function updateAllRestaurant(id: number) {
+    try {
+        //Creating a variable that will contain the data that we need from the endpoint API
+        const response = await axiosInstance.put<ApiResponse>(`/restaurant/update/${id}`);
+        console.log("Response: ",response)
+        return response.data;
+    } catch (error) {
+        console.log('error: ', error);
+        throw new Error('Failed to fetch data');
+    }
+}
+
+export async function deleteAllRestaurant(id: number) {
+    try {
+        //Creating a variable that will contain the data that we need from the endpoint API
+        const response = await axiosInstance.delete<ApiResponse>(`/restaurant/delete/${id}`);
+        console.log("Response: ",response)
+        return response.data;
+    } catch (error) {
+        console.log('error: ', error);
+        throw new Error('Failed to fetch data');
+    }
+}
+
--- Food-Delivery-FrontEnd/Food-Delivery/src/types/Restaurant.tsx
+++ Food-Delivery-FrontEnd/Food-Delivery/src/types/Restaurant.tsx
@@ -1,10 +1,56 @@
-export interface Restaurant {
-    category: string[];
+interface Restaurant {
+    id: number;
     name: string;
-    discount: number;
+    address: string;
+    menu: Menu[];
+    openingHours: string[];
+    phoneNumber: string;
+    website: string;
+    averageRating: number;
+    isOpen: boolean;
+    reviews: Review[];
     images: string[];
-}
+    restaurantManager: User;
+    discount: number;
+    order: Order[];
+    category: string[];
+    restaurantPayments: RestaurantPayment[];
+    isApproved: boolean;
+    isActive: boolean;
+    revenue: Revenue;
+    documentation: Documentation;
+  }
+  
+  interface Menu {
+    // Define properties of Menu here
+  }
+  
+  interface Review {
+    // Define properties of Review here
+  }
+  
+  interface User {
+    // Define properties of User here
+  }
+  
+  interface Order {
+    // Define properties of Order here
+  }
+  
+  interface RestaurantPayment {
+    // Define properties of RestaurantPayment here
+  }
+  
+  interface Revenue {
+    // Define properties of Revenue here
+  }
+  
+  interface Documentation {
+    // Define properties of Documentation here
+  }
 
+  
+  
 type HandleRestorantsFunction = (category: string) => void;
 
 export interface NavBarProps {

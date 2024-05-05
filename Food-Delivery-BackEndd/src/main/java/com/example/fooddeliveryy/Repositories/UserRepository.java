5cfb2b9ec50661502fdb7b22d84b9d2ba33c54f6
greisi jaho <gjaho21@epoka.edu.al>
2024-05-06 01:29:27 +0200

    User controller

--- Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Repositories/UserRepository.java
+++ Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.fooddeliveryy.Repositories;
 
+import com.example.fooddeliveryy.Entities.Enums.UserRole;
 import com.example.fooddeliveryy.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserEmail(String email);
     Optional<User> findByUserId(long id);
     boolean existsByUserEmail(String email);
+    List<User> findByUserRole(UserRole userRole);
 }
--- Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Services/UserService.java
+++ Food-Delivery-BackEndd/src/main/java/com/example/fooddeliveryy/Services/UserService.java
@@ -1,15 +1,39 @@
 package com.example.fooddeliveryy.Services;
 
+import com.example.fooddeliveryy.Entities.Enums.UserRole;
+import com.example.fooddeliveryy.Entities.Rastaurant;
 import com.example.fooddeliveryy.Entities.User;
 import com.example.fooddeliveryy.Configuration.JWT.JwtTokenProvider;
+import com.example.fooddeliveryy.Repositories.RestaurantRepo;
+import com.example.fooddeliveryy.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class UserService {
-    private final JwtTokenProvider jwtTokenProvider;
+
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserService(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-        User.setJwtTokenProvider(jwtTokenProvider);
+    public UserService( UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+    }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public List<User> getUsersByRole(UserRole userRole) {
+        return userRepository.findByUserRole(userRole);
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(long userId) {
+            userRepository.deleteById(userId);
     }
 }

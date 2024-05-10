e6adb71ec26401e717425ec5791139d8da1779e7
greisi jaho <gjaho21@epoka.edu.al>
2024-05-10 18:25:44 +0200

    DeleteRestaurant api

--- Food-Delivery-FrontEnd/Food-Delivery/src/api/localhost/Administrator/restaurantsApi.tsx
+++ Food-Delivery-FrontEnd/Food-Delivery/src/api/localhost/Administrator/restaurantsApi.tsx
@@ -84,3 +84,15 @@ export async function getNotApprovedRestaurants() {
         throw new Error('Failed to fetch data');
     }
 }
+
+export async function deleteRestaurant(name: string) {
+    try {
+        //Creating a variable that will contain the data that we need from the endpoint API
+        const response = await axiosInstance.delete<ApiResponse>(`/restaurant/delete/${name}`);
+        console.log('Response: ', response);
+        return response.data;
+    } catch (error) {
+        console.log('error: ', error);
+        throw new Error('Failed to fetch data');
+    }
+}

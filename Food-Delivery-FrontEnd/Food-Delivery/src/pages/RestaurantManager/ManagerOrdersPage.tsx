d349a1d47f1ba3bcedd8dcf86bbfa63c0ecec8d1
greisi jaho <gjaho21@epoka.edu.al>
2024-05-15 11:44:42 +0200

    Creating specific order

--- Food-Delivery-FrontEnd/Food-Delivery/src/pages/RestaurantManager/ManagerOrdersPage.tsx
+++ Food-Delivery-FrontEnd/Food-Delivery/src/pages/RestaurantManager/ManagerOrdersPage.tsx
@@ -2,14 +2,14 @@ import './style/manager.css';
 import NavBarManager from 'src/component/RestaurantManager/NavBarManager';
 import SideBarManager from 'src/component/RestaurantManager/SideBarManager';
 import AllOrders from 'src/component/RestaurantManager/AllOrders';
-
+import SpecificOrderCard from 'src/component/RestaurantManager/SpecificOrderCard';
 function ManagerOrdersPage() {
     return (
         <div>
             <div className="ml-[280px] sideBarClose h-[100vh]">
                 <NavBarManager />
-                <div className="m-[20px]">
-                    <AllOrders />
+                <div className="m-[20px] ">
+                    <SpecificOrderCard />
                 </div>
             </div>
             <div className="ml-[280px] mt-[100px]">
